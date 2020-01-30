8e7fe1e038be53efc833e2029c3b1817c744a870
shalkovy <shalkovy@gmail.com>
2020-01-30 21:31:31 +0300

    new project init

---
+++ TextureExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextureExample
+//
+//  Created by Dima Shelkov on 1/30/20.
+//  Copyright © 2020 Dima Shelkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
