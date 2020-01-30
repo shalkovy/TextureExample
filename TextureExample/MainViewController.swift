807e3996d44d6b210d82c176c72a1e0548d5355b
shalkovy <shalkovy@gmail.com>
2020-01-30 21:38:06 +0300

    clean code

--- TextureExample/MainViewController.swift
+++ TextureExample/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  TextureExample
 //
 //  Created by Dima Shelkov on 1/30/20.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
